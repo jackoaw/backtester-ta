86423a590638e09d947c70b75c59bf1c718d436c
jackoaw <worldchuba@gmail.com>
2023-02-09 20:43:20 -0500

    added slow ema and some brute force rsi trades

--- layout.py
+++ layout.py
@@ -1,12 +1,14 @@
 import pandas as pd
 from ta import add_all_ta_features
 from ta.utils import dropna
+import np
 
 global_starting_money = 10000
 
 class Strategy:
 
 	def __init__(self, 
+		name,
 		conditions, 
 		# close_conditions, 
 		starting_money=global_starting_money,
@@ -21,6 +23,7 @@ class Strategy:
 		self.account = Account(starting_money, risk_percent)
 		self.open_args = open_args
 		self.close_args = close_args
+		self.name = name
 		# self.currentLong
 
 	def act(self, i, row):
@@ -55,16 +58,16 @@ class Account:
 		# If short, close
 		elif self.holdnum < 0:
 			self.money -= (price*self.holdnum*-1)
-			print("Closed %i shares at price %f"%(self.holdnum, price))
+			# print("Closed %i shares at price %f"%(self.holdnum, price))
 			self.holdnum = 0
 		# Else start long
 		else:
 			buyammount = int((self.money*risknum)/price)
 			self.money -= (price*buyammount)
 			self.holdnum = buyammount
-			print("Bought %i shares at price %f"%(buyammount, price))
+			# print("Bought %i shares at price %f"%(buyammount, price))
+		# print("Money: %f"%self.money)
 
-		print("Money: %f"%self.money)
 
 	def sell(self, price, risknum=0):
 		risknum = self.risknum if risknum==0 else .2
@@ -74,16 +77,16 @@ class Account:
 		# If long, close
 		elif self.holdnum > 0:
 			self.money += (price*self.holdnum)
-			print("Closed %i shares at price %f"%(self.holdnum, price))
+			# print("Closed %i shares at price %f"%(self.holdnum, price))
 			self.holdnum = 0
 		# Else start short
 		else:
 			buyammount = int(((self.money*risknum)/price))
 			self.money += (price*buyammount)
 			self.holdnum = buyammount*-1
-			print("Sold %i shares at price %f"%(buyammount, price))
+			# print("Sold %i shares at price %f"%(buyammount, price))
 
-		print("Money: %f"%self.money)
+		# print("Money: %f"%self.money)
 
 
 # Needs arg 
@@ -92,20 +95,23 @@ class Account:
 def rsi(row, i, args={}, **kwargs):
 	if args['lt']:
 		if row['momentum_rsi'] <= args["rsinum"]:
-			print("RSI Value: %f" %row['momentum_rsi']) 
 			return True
 	else:
 		if row['momentum_rsi'] >= args["rsinum"]:
-			print("RSI Value: %f" %row['momentum_rsi']) 
 			return True
+	return False
 
 
-# def rsilow(row, i, rsinum=30, args={}, **kwargs):
-# 	# if int(df.at[i, 'momentum_rsi']) > rsinum:
-# 	# 	return True
-# 	if row['momentum_rsi'] < rsinum:
-# 		print("RSI Value: %f" %row['momentum_rsi']) 
-# 		return True
+def ema_slow(row, i, args={}, **kwargs):	
+	if row.isna()['trend_ema_slow'] is True:
+		return False
+	if not args['above']:
+		if row['Close'] <= row["trend_ema_slow"]:
+			return True
+	else:
+		if row['Close'] >= row["trend_ema_slow"]:
+			return True
+	return False
 
 # Combo one buy method with one sell method?
 
@@ -121,10 +127,21 @@ def main():
 		df, open="Open", high="High", low="Low", close="Close", volume="Volume"
 	)
 
+	strats = []
+
+	# Form all of the RSI Trades
+	for x in range(0,100):
+		for y in range(0,100):
+			strats.append(
+				Strategy("RSI Buy %i Sell %i"%(y, x), (rsi, rsi), open_args={"rsinum":x, 'lt':True, 'buy':False}, close_args={"rsinum":y, 'lt':False, 'buy':True})
+			)
+
+
 	# Form all of the strategies here
-	strats = [
-		Strategy((rsi, rsi), open_args={"rsinum":70, 'lt':True, 'buy':False}, close_args={"rsinum":60, 'lt':False, 'buy':True}),
-		Strategy((rsi, rsi), open_args={"rsinum":30, 'lt':True, 'buy':True}, close_args={"rsinum":60, 'lt':False, 'buy':False})
+	strats_example = [
+		Strategy("RSI High", (rsi, rsi), open_args={"rsinum":70, 'lt':True, 'buy':False}, close_args={"rsinum":60, 'lt':False, 'buy':True}),
+		Strategy("RSI Low", (rsi, rsi), open_args={"rsinum":30, 'lt':True, 'buy':True}, close_args={"rsinum":60, 'lt':False, 'buy':False}),
+		Strategy("Slow EMA Trade", (ema_slow, ema_slow), open_args={"above":False, 'buy':False}, close_args={"above":True, 'buy':True})
 	]
 
 	# Cycle through buy_methods and check success rate
@@ -134,15 +151,25 @@ def main():
 			strat.act(i, row)
 		lastrow = row
 
+	success_strategies = []
+
 	for strat in strats:
 		# Close the account first
 		if strat.account.holdnum > 0:
 			strat.account.sell(lastrow['Close'])
 		elif strat.account.holdnum < 0:
 			strat.account.buy(lastrow['Close'])
-		print("Account Money: %f"%strat.account.money)
+
 		percent_change = (strat.account.money/global_starting_money - 1)*100
-		print("This strategy generated a %0.2f%% return"% percent_change)
+		if percent_change >= 1:
+			# print("Account Money: %f"%strat.account.money)
+			# print("Strategy %s generated a %0.2f%% return"% (strat.name, percent_change))
+			success_strategies.append(strat)
+
+	sorted_success_strategies = sorted(success_strategies, key=lambda x: x.account.money, reverse=True)
+
+	for strat in success_strategies:
+		print("Strategy %s generated a %0.2f%% return"% (strat.name, percent_change))
 
 	# Final step is to close all account positions, or just report the total
 
